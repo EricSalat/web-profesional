ecfdaeb5924128d465c9fc578fd78da0f0dc9e77
EricSalat <ericsalat@protonmail.com>
2022-11-28 18:57:27 +0100

    Corrección textos biografia

--- src/App.js
+++ src/App.js
@@ -47,8 +47,8 @@ function App() {
           <NumberedHeading texto="Sobre mi" />
           <div className="about-me">
             <div className="about-me-text">
-              <p>Soy un perfil híbrido. Combino mi formación técnica en programación web con el conocimiento en marketing y comunicación. Estas disciplinas me dan una visión global de los proyectos, desde la creación hasta la monetización. </p>
-              <p>Enfoco en diseño y el desarrollo web en la mejora de los embudos de tráfico y la conversión de leads y ventas. Mi bagaje polivalente me ha dado habilidades de oratoria, trabajo en equipo, reuniones con clientes y capacidad analítica de métricas y datos.</p>
+              <p>¡Hola! Me llamo Èric, vivo en Barcelona y me encanta crear cosas que vivan en Internet. Todo empezó en el año 2020 cuando entré a un programa para que jóvenes emprendedores aprendieran a crear aplicaciones móviles y crearan startups tecnológicas. Allí me metí de lleno a aprender desarrollo web.</p>
+              <p>En la universidad estudié comunicación y marketing, que combinados con mis conocimientos en programación, me permiten ser un perfil híbrido con una visión transversal de los proyectos. Por ello, enfoco el Front-end no solo al desarrollo, sino en la mejora de embudos de tráfico y la conversión de leads y ventas.</p>
               <p>Estas son las tecnologías con las que he estado trabajando:</p>
               <div>
                 <ul className="skills-list">
@@ -59,6 +59,10 @@ function App() {
                   <li>JavaScript (ES6+)</li>
                   <li>React</li>
                   <li>PHP</li>
+                  <li>Google Ads</li>
+                  <li>Google Analytics</li>
+                  <li>Amazon Ads</li>
+
                 </ul>
               </div>
             </div>
