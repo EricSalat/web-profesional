a40866f56f41cdce2374f38517e266358929d046
ericsalat <ericsalat@protonmail.com>
2023-07-18 12:54:20 +0200

    Añadido Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="es">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   )
 }
